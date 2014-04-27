d4bab5696fded2ee2f3a9329c3fb9c25f838ba33
Hadoop <hadoop@HadoopPC.(none)>
2014-04-26 19:07:20 -0500

    Changed to way I was checking the origin and ids

--- reducer.py
+++ reducer.py
@@ -13,35 +13,36 @@ for line in sys.stdin:
 
     # parse the input we got from mapper.py
     #id, ymdh, user_id, clicks, query_string, origin = line.split(',')
-    id, value = line.split('\t')
+    region_id, value = line.split('\t')
     ymdh, user_id, clicks, query_string, origin = value.split(',')
     x = "%s,%s,%s,%s" % (ymdh, user_id, clicks, query_string)
-
-    if origin == 'S':
+    #print "origin: %s" % (origin)
+    if 'S' in origin:
       s_tupes.add(x)
     else:
       t_tupes.add(x)
 
-s_tupes = list(s_tupes)
-t_tupes = list(t_tupes)
+#s_tupes = list(s_tupes)
+#t_tupes = list(t_tupes)
 for s in s_tupes:
   for t in t_tupes:
     s_ymdh, s_user_id,  s_clicks, s_query_string = s.split(',')
     t_ymdh, t_user_id,  t_clicks, t_query_string = t.split(',')
     #print "CLICKS: %s, %s" % (s_clicks, t_clicks)
-    if s_clicks == '1' and t_clicks == '1':
-      print "COUNT FOUND"
+    if '1' in s_clicks and '1' in t_clicks:
+      #print "COUNT FOUND"
       if s_user_id != t_user_id:
-        print "different ids found"
+        #print "different ids found"
         s_sec = s_ymdh.split(' ')[1].split(':')[-1]
         t_sec = t_ymdh.split(' ')[1].split(':')[-1]
         if abs(int(s_sec) - int(t_sec)) < 2:
-          print "other found"
+          #print "other found"
           #output.add("%s,%s,%s" % (s_ymdh, s_query_string, t_query_string))
           print "%s,%s,%s" % (s_ymdh, s_query_string, t_query_string)
 
 #for x in output:
   #print x 
+
 #print "s_tupes"
 #print s_tupes
 
