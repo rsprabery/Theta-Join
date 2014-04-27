c0980da8991abbdfb41465be68e2f3349413b147
read <read.sprabery@gmail.com>
2014-04-26 19:17:32 -0500

    Better filtering in mapper

--- mapper.py
+++ mapper.py
@@ -15,33 +15,36 @@ for line in sys.stdin:
   # split the line into words
   columns = line.split(',')
   output = []
-  output.append(columns[0])
-  output.append(columns[1])
-  output.append(columns[3])
-  output.append(columns[19])
 
-  # Since this is a self join, then x is in both S & T
+  # ignore if there are no clicks
+  if '1' in columns[3]:
+    output.append(columns[0]) # ymdh
+    output.append(columns[1]) # user_id
+    output.append(columns[3]) # clicks
+    output.append(columns[19]) # query_string
 
-  # S part
-  matrixRowS = random.randint(1, height)
-  begin_S_ID = (matrixRowS * (matrixRowS -1)) + 1
-  # ERROR HERE
-  end_S_ID = (matrixRowS * (matrixRowS)) + 1
-  #rowSIDS = range(begin_S_ID, end_S_ID)
+    # Since this is a self join, then x is in both S & T
 
-  rowSIDS = range((matrixRowS -1) * width + 1, matrixRowS * width + 1)
+    # S part
+    matrixRowS = random.randint(1, height)
+    begin_S_ID = (matrixRowS * (matrixRowS -1)) + 1
+    # ERROR HERE
+    end_S_ID = (matrixRowS * (matrixRowS)) + 1
+    #rowSIDS = range(begin_S_ID, end_S_ID)
 
-  for region_id in rowSIDS:
-    print '%s\t%s,S' % (region_id, ','.join(output))
+    rowSIDS = range((matrixRowS -1) * width + 1, matrixRowS * width + 1)
 
-  # T Part
-  rows = range(1, height)
-  matrix_column = random.randint(1, width)
-  column_T_ids = []
-  for row in rows:
-    region_id = ((row - 1) * width) + matrix_column
-    column_T_ids.append(region_id)
+    for region_id in rowSIDS:
+      print '%s\t%s,S' % (region_id, ','.join(output))
 
-  for region_id in column_T_ids:
-    print '%s\t%s,T' % (region_id, ','.join(output))
+    # T Part
+    rows = range(1, height)
+    matrix_column = random.randint(1, width)
+    column_T_ids = []
+    for row in rows:
+      region_id = ((row - 1) * width) + matrix_column
+      column_T_ids.append(region_id)
+
+    for region_id in column_T_ids:
+      print '%s\t%s,T' % (region_id, ','.join(output))
 
